7aaf9788fac4fd862520decf5b5431d6150131d5
Aditya17-4 <58966618+Aditya17-4@users.noreply.github.com>
2021-08-21 13:47:45 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-	const APP_ID = "cf11db71 ";
-	const APP_KEY = "f294a9f910f56f33e981a252491e6924";
-	const examplereq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-
-	const [counter, setCounter] = useState(0);
+	const APP_ID = "41fd9550";
+	const APP_KEY = "952c800fb6b5309aebdcfcda51b64045	";
 
 	useEffect(() => {
-		console.log("effect in place");
-	});
+		getRecipes();
+	}, []);
 
+	const getRecipes = async () => {
+		const response = await fetch(
+			`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+		);
+		const data = await response.json();
+		console.log(data);
+	};
 	return (
 		<div className="App">
 			<form className="search-form">
 				<input className="search-bar" type="text" />
-				<button
-					onClick={() => setCounter(counter + 1)}
-					className="search-button"
-					type="submit">
-					{counter}
+				<button className="search-button" type="submit">
+					Search
 				</button>
 			</form>
 		</div>
